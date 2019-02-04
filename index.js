4a920a989cc8a1cd002d95d8e6018e4666aeaf50
Jeff <j_lee92@hotmail.com>
2019-02-03 21:11:47 -0500

    Ready

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
-mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true});
+mongoose.connect(
+  "mongodb://jefflee:testing1@ds119085.mlab.com:19085/recipes",
+  { useNewUrlParser: true }
+);
 
 // App Setup
 
 app.use(morgan('combined')); // express middleware. Morgan is a logging framework
+app.use(cors());
 app.use(bodyParser.json({ type:'*/*'})); // express middleware. Used to parse incoming requests into json, no matter what the request type is.
 router(app);
 
