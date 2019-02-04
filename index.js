b7418bb5cdd3969a45eb4794e24967e1a10e360f
Jeff <j_lee92@hotmail.com>
2019-02-04 17:04:06 -0500

    port change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('*', (req, res) => {
 // DB Setup
 
 const DBURI = process.env.MONGODB_URI || "mongodb://jefflee:testing1@ds119085.mlab.com:19085/recipes";
-mongoose.connect(DBURI ,
+mongoose.connect(DBURI,
   { useNewUrlParser: true }
 );
 
